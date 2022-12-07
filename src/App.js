9e7e8b991a5eff787d47735fd1b03f6931b68c5a
SAURABH694 <singhsaurabh9574@gmail.com>
2022-12-06 19:54:46 -0800

    dropdown

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+
+  <>
+  <h3>Should you use a dropdown?</h3>
+  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown
+  </button>
+  <ul class="dropdown-menu">
+    <li><button class="dropdown-item" type="button">Yes</button></li>
+    <li><button class="dropdown-item" type="button">Probably Not</button></li>
+  
+  </ul>
+</div>
+</>
+    
+  
+  );
+}
+
+export default App;
